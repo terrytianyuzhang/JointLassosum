c9611da507c4d5151d5152846f0795b53c30ee07
Tianyu Zhang 张天宇 <zty@uw.edu>
2023-12-01 13:56:26 -0500

    Clean up

--- JLS_basic/code/fit_JLS.R
+++ JLS_basic/code/fit_JLS.R
@@ -57,13 +57,14 @@ small_population_type <- 'YRI'
 ###HYPERPARAMETER CANDIDATES
 JLS_population_weight <- c(0.5, 0.8)
 # JLS_l1_penalty <- exp(seq(log(0.007), log(0.05), length.out=5))
-JLS_l1_penalty <- exp(seq(log(0.1), log(0.05), length.out = 2))
+JLS_l1_penalty <- exp(seq(log(0.01), log(0.005), length.out = 2))
 JLS_shrinkage <- c(0.75)
 chromosome <- 21:22
-mem.limit <- 2e10
+mem.limit <- 2*10e9
+mymc.cores <- 8
 
 ###GIVEN THE ABOVE INFORMATION, FIT THE MODEL FOR ONE TIME
-JLS_population_weight_one <- JLS_population_weight[1]
+JLS_population_weight_one <- JLS_population_weight[2]
 
 
 
@@ -152,63 +153,10 @@ save(JLS_result_one_weight,
 
 ###PARALLEL
 
-
-
-
-# setting.title <- 'CEU1aYRI2a22Chr'
-##### general setup
-# i.sim="NC8000"
-# load(paste0("Work/Sim-C0.80-Y0.60-rep",i.sim,"/simulation-params.RData"))
-# load("/raid6/Ron/prs/data/bert_sample/simulation-params.RData")
-
-# set directories for this simulation
-# main.dir=params$run.info$main.dir
-# work.dir=params$run.info$work.dir
-
-# load the parameters for this simulation
-# load(paste0("Work/Sim-",i.sim,"/simulation-params.RData"))
-# main.dir=params$run.info$main.dir #"/raid6/Tianyu/PRS/SimulationPipeline/"
-# work.dir=params$run.info$work.dir #"/raid6/Tianyu/PRS/SimulationPipeline/Work/Sim-800/"
-
-### ancestries
-gwasANC=c("CEU","YRI")
-# #### set the parameters
-# GAMMA = c(0.2, 0.5, 0.8)
-# ###!!!!!
-# lambda=exp(seq(log(0.0025), log(0.025), length.out=10))
-# lambda[1] <- 0.00025
-# lambda=exp(seq(log(0.007), log(0.05), length.out=10))  ##try larger lambda
-
-# lambda=exp(seq(log(0.001), log(0.025), length.out=10))
-# shrink=.9
-
-####sample sizes
-# N1 <- params$CEU.TRN$n.case + params$CEU.TRN$n.control
-# N2 <- params$YRI.TRN$n.case + params$YRI.TRN$n.control
-N1 <- sample_sizes$CEU$n.case + sample_sizes$CEU$n.control
-N2 <- sample_sizes$YRI$n.case + sample_sizes$YRI$n.control
-input.df <- data.frame(gamma = GAMMA,
-                       N1 = rep(N1, length(GAMMA)),
-                       N2 = rep(N2, length(GAMMA)))
-###!!!!this is not correct
-# N=seq(20000,20000,4000)
-# input.df=data.frame(N1=rep(N,each=length(N)),N2=rep(N,length(N)))
-# input.df=data.frame(gamma=rep(GAMMA,each=nrow(input.df)),N1=rep(input.df$N1,length(GAMMA)),N2=rep(input.df$N2,length(GAMMA)))
-# 
-# ###this is the correct sample size
-# input.df <- data.frame(gamma = GAMMA, N1 = 20000, N2 = 4000)
-# memory limit
-mem.limit <- 2*10e9
-
-# end of the wrapper function
-
 system.time(re.wrapper<-mclapply(1:nrow(input.df),wrapperFunction,input.df=input.df,
                                  gwasANC = gwasANC, lambda=lambda, shrink=shrink,
                                  main.dir=main.dir,work.dir=work.dir,CHR= 1:22, mem.limit=2e10,
                                  mc.cores=3, mc.preschedule = F, mc.silent=F))
 
-rm.list=ls()
-rm.list=rm.list[!(rm.list %in% c("i.sim","sims", "plink", "plink2"))]
-rm(list = rm.list); flush.console()
 
 
